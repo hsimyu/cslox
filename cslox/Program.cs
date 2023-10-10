9d3841e4c72aa3e897484323a6900b0190e80aa1
hsimyu <hsimyu1900@gmail.com>
2023-10-10 23:46:06 +0900

    Add stub solution.

---
+++ cslox/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace cslox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage: cslox [script/file/path.cslox]");
+                Environment.Exit(1);
+            }
+            else if (args.Length == 1)
+            {
+                runFile(args[0]);
+            }
+            else
+            {
+                runPrompt();
+            }
+        }
+
+        static void runFile(string filename)
+        {
+            // TODO: 存在チェック
+            var script = File.ReadAllText(filename);
+            runImpl(script);
+        }
+
+        static void runPrompt()
+        {
+            // 入力を受け取る
+        }
+
+        static void runImpl(string script)
+        {
+            Console.WriteLine(script);
+        }
+    }
+}

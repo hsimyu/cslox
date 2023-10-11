05cc93f7f0dc58636736dac7d3ae2481d0c397ce
hsimyu <hsimyu1900@gmail.com>
2023-10-11 23:25:21 +0900

    Add token type.

---
+++ cslox/Token.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cslox
+{
+    internal class Token
+    {
+        TokenType type;
+        string lexeme;
+        object literal;
+        int line;
+
+        Token(TokenType type, string lexeme, object literal, int line)
+        {
+            this.type = type;
+            this.lexeme = lexeme;
+            this.literal = literal;
+            this.line = line;
+        }
+
+        public override string ToString()
+        {
+            return $"{type} {lexeme} {literal}";
+        }
+    }
+}
---
+++ cslox/TokenType.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cslox
+{
+    enum TokenType
+    {
+        // 記号 1 つのトークン
+        LEFT_PAREN,
+        RIGHT_PAREN,
+        LEFT_BRACE,
+        RIGHT_BRACE,
+        COMMA,
+        DOT,
+        MINUS,
+        PLUS,
+        SEMICOLON,
+        SLASH,
+        STAR,
+
+        // 記号1つまたは2つのトークン
+        BANG,
+        BANG_EQUAL,
+        EQUAL,
+        EQUAL_EQUAL,
+        GREATER,
+        GREATER_EQUAL,
+        LESS,
+        LESS_EQUAL,
+
+        // リテラル
+        IDENTIFIER,
+        STRING,
+        NUMBER,
+
+        // キーワード
+        AND,
+        CLASS,
+        ELSE,
+        FALSE,
+        FUN,
+        FOR,
+        IF,
+        NIL,
+        OR,
+        PRINT,
+        RETURN,
+        SUPER,
+        THIS,
+        TRUE,
+        VAR,
+        WHILE,
+
+        EOF,
+    }
+}

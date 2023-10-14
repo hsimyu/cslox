de9b3a2d1455dd8d88683fd1ad4b267221e382b4
hsimyu <hsimyu1900@gmail.com>
2023-10-14 23:44:46 +0900

    Impl Binary Expression.

---
+++ cslox/Expression.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cslox
+{
+    abstract class Expression
+    {
+    }
+
+    class Binary : Expression
+    {
+        Binary(Expression left, Token op, Expression right)
+        {
+            this.left = left;
+            this.op = op;
+            this.right = right;
+        }
+
+        Expression left;
+        Token op;
+        Expression right;
+    }
+}

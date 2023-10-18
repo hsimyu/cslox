646c906f2b5149d4a00996fb8bd8ee35406441e9
hsimyu <hsimyu1900@gmail.com>
2023-10-18 23:48:54 +0900

    Impl initial Parser.

---
+++ cslox/Parser.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cslox
+{
+    public class Parser
+    {
+        List<Token> tokens = new List<Token>();
+        int currentIndex = 0;
+
+        public Parser(List<Token> tokens)
+        {
+            this.tokens = tokens;
+        }
+
+        Expression expression()
+        {
+            // expression := equality
+            return equality();
+        }
+
+        Expression equality()
+        {
+            // equality := comparison ( ("!=" | "==") comparison )*
+            var lhs = comparison();
+
+            while (match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL))
+            {
+                Token op = previous();
+                Expression rhs = comparison();
+                lhs = new Expression.Binary(lhs, op, rhs);
+            }
+
+            return lhs;
+        }
+
+        Expression comparison()
+        {
+            return null;
+        }
+
+        bool match(params TokenType[] types)
+        {
+            foreach (var t in types)
+            {
+                if (check(t))
+                {
+                    advance();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        Token advance()
+        {
+            if (!isAtEnd()) currentIndex++;
+            return previous();
+        }
+
+        bool check(TokenType type)
+        {
+            if (isAtEnd()) return false;
+            return peek().type == type;
+        }
+
+        bool isAtEnd()
+        {
+            return peek().type == TokenType.EOF;
+        }
+
+        Token peek()
+        {
+            return tokens[currentIndex];
+        }
+
+        Token previous()
+        {
+            return tokens[currentIndex - 1];
+        }
+    }
+}

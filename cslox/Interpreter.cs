4c758f22b069d648c671fdefbfcb2fd05d2ff559
hsimyu <hsimyu1900@gmail.com>
2023-10-21 22:38:49 +0900

    Add Interpreter class.

---
+++ cslox/Interpreter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cslox
+{
+    public class Interpreter : Expression.IVisitor<object?>
+    {
+        public object? visitBinary(Expression.Binary expression)
+        {
+            return null;
+        }
+
+        public object? visitTernary(Expression.Ternary expression)
+        {
+            return null;
+        }
+
+        public object? visitGrouping(Expression.Grouping expression)
+        {
+            return null;
+        }
+
+        public object? visitLiteral(Expression.Literal expression)
+        {
+            return expression.value;
+        }
+
+        public object? visitUnary(Expression.Unary expression)
+        {
+            return null;
+        }
+    }
+}
